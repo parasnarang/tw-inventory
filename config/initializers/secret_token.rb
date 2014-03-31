4d8a2b6dc145a21479c8e0476e4f33f632dff686
Paras <narangparas@gmail.com>
2014-03-31 12:26:25 +0530

    Rails4 app without activerecord and test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwInventory::Application.config.secret_key_base = '2f343cf92b0e701898941cc0bfb8aeaf01fe2f5c103195abc4a942703638b84d57dd0b6180e141186026ecfa7a403fd2262a5745d199a09ca341437fdb85df7b'
