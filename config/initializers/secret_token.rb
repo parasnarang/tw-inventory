671af8f735c5dc71aa3e5191f18e084b197d3fac
Paras <narangparas@gmail.com>
2014-03-31 11:54:48 +0530

    Initial rails4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwInventory::Application.config.secret_key_base = '1e8e886369f9338617f1f9f5336354ba3a042646e7e9a81651639b8fe0d455ee49e370e81626a7ccc474b8c90b50bc187a5226f5fe1f83cc1f449aa105e81fbb'
