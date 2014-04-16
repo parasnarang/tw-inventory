dff895032a0cfd848a1eb3a19755b033f66c8c46
Vishal Sharma <vishalsh@thoughtworks.com>
2014-04-16 12:59:13 +0530

    Made roles field a dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,13 @@ class User
   field :name, type: String
   field :role, type: String
 
-  validates_presence_of :ad_id
-  validates_presence_of :name
+  validates_presence_of :ad_id, :name, :role
 
   has_many :assignments
+
+  def role_enum
+    [['admin'],['employee']]
+  end
 
   def self.admin?(ad_id)
     u = User.where(:ad_id => ad_id)
